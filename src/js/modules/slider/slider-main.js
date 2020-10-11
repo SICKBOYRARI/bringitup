0934def96f8e85fb9553533b8563011415dc9d9e
SICKBOYRARI <jediz0rrr@gmail.com>
2020-10-12 03:21:57 +0700

    Initialized main slider of second page

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -40,31 +40,50 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
+    bindTriggers() {
+        this.btns.forEach(item => {
+            item.addEventListener('click', () => {
+                this.plusSlides(1);
+                this.slides[this.slideIndex -1].classList.add('animated', 'slideInDown');
+            });
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+                this.slides[this.slideIndex -1].classList.add('animated', 'slideInDown');
+            });
+        });
+
+        document.querySelectorAll('.prevmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1);
+                this.slides[this.slideIndex -1].classList.add('animated', 'slideInRight');
+            });
+        });
+
+        document.querySelectorAll('.nextmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1);
+                this.slides[this.slideIndex -1].classList.add('animated', 'slideInLeft');
+            });
+        });
+    }
+
     render() {
-        try{
+        if (this.container) {
             try {
                 this.hanson = document.querySelector('.hanson');
             } catch(e) {
 
             }
 
-            this.btns.forEach(item => {
-                item.addEventListener('click', () => {
-                    this.plusSlides(1);
-                    this.slides[this.slideIndex -1].classList.add('animated', 'slideInDown');
-                });
-
-                item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.slideIndex = 1;
-                    this.showSlides(this.slideIndex);
-                    this.slides[this.slideIndex -1].classList.add('animated', 'slideInDown');
-                });
-            });
-
             this.showSlides(this.slideIndex);
-        } catch (e) {
-
+            this.bindTriggers();
         }
     }
 }
